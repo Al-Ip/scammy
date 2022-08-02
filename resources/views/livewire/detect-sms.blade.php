91ddcda515b6154ac301a38373acceb97dd2f023
Alexandru C Ipsalat <alexipsalat@hotmail.com>
2022-08-02 22:52:29 +0100

    Foundation design added

---
+++ resources/views/livewire/detect-sms.blade.php
@@ -0,0 +1,30 @@
+<div class="chat">
+    <div class="yours messages">
+        <div class="message">
+            Hey!
+        </div>
+        <div class="message">
+            Not sure if your text could be spam?
+        </div>
+        <div class="message last">
+            Enter it below and find out &#128269;
+        </div>
+    </div>
+    <div class="mine messages">
+        <div class="message">
+            What response message will look like
+        </div>
+        <div class="message last">
+            Result in here... etc
+        </div>
+    </div>
+    <div class="flex justify-end items-end mb-3">
+        <span class="cursor-pointer overflow-hidden max-h-40 block p-2.5 mt-5 w-full text-sm text-gray-700 bg-gray-50 rounded-2xl border focus:outline-none
+            border-purple-400 focus:ring-pink-600 focus:border-pink-600 dark:bg-purple-100 dark:border-purple-400 dark:placeholder-gray-400
+            dark:text-black dark:focus:ring-pink-600 dark:focus:border-pink-600"
+            placeholder="Your message..." role="textbox" contenteditable> <p class="p-2">Enter message to check here... </p>
+        </span>
+        <x-send-sms class="w-auto h-14 p-1 text-pink-600 cursor-pointer hover:text-purple-400" />
+    </div>
+    <button>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
 
 module.exports = {
     theme: {
         extend: {
             fontFamily: {
                 sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                slate: colors.slate,
             },
         },
     },
